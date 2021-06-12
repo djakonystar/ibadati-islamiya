30bf00aa5f6843d01926f65f8c4b4b59a39d1f28
djakonystar <djakonystar911@gmail.com>
2021-06-12 12:43:32 +0500

    fullscreen in splash

--- app/src/main/java/uz/bismillah/ibadatiislamiya/ui/SplashActivity.kt
+++ app/src/main/java/uz/bismillah/ibadatiislamiya/ui/SplashActivity.kt
@@ -18,7 +18,12 @@ class SplashActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
 
         texnoposLogo.setMinAndMaxFrame(0, 357)
